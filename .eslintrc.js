2dd1805a8fd8181c34cd3fcaf3beaf787f7a4e72
Mukhammad Sobirov <mukhammadsobirov.ms@gmail.com>
2023-03-02 17:14:14 +0300

    chore: set up prettier and eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,13 +5,14 @@ module.exports = {
   },
   extends: 'standard-with-typescript',
   overrides: [],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
   rules: {
-    '@typescript-eslint/no-unused-vars': 'error',
-    // to enforce using type for object type definitions, can be type or interface
-    '@typescript-eslint/consistent-type-definitions': ['error', 'type']
+    '@typescript-eslint/no-unused-vars': 'error'
   }
 }
